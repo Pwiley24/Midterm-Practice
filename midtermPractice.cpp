4a851417db8d09d7f1151e5022f15150f42ee18f
U-BSD\398256 <398256@SHS-DGDRCS2.beaverton.k12.or.us>
2022-10-24 14:24:12 -0700

    setup for the problem

---
+++ midtermPractice.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  char input[50];
+  char newString[50];
+  cin >> input;
+  cin.ignore(50, '\n');
+
+  
+
+  return 0;
+}
+
+void switchLetters(&input, &newString){
+  
+}
